4df89731747c02ae800152bd9eaa952e9b4eab6e
Lukas Lorenc <lorenc.lucas@gmail.com>
2023-06-13 00:11:21 +0200

    add basic version of the app

--- WeatherApp/src/app/app.component.ts
+++ WeatherApp/src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'WeatherApp';
+  cityName = '';
+  weatherData: any = null;
+  cityData: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  searchCity() {
+    this.http
+      .get<any[]>(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&limit=1&appid=4e0f62859c4b83cad567e0bd3a648de1`
+      )
+      .subscribe({
+        next: (data) => {
+          if (data.length > 0) {
+            const { lat, lon } = data[0];
+            this.cityData = data[0];
+            this.getWeatherData(lat, lon);
+          } else {
+            console.log('City not found');
+          }
+        },
+        error: (error) => {
+          console.log('Error occurred while fetching city data');
+        },
+      });
+  }
+
+  getWeatherData(lat: number, lon: number) {
+    this.http
+      .get<any>(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=4e0f62859c4b83cad567e0bd3a648de1&exclude=minutely,daily,alerts,hourly&units=metric`
+      )
+      .subscribe({
+        next: (data) => {
+          this.weatherData = data;
+        },
+        error: (error) => {
+          console.log('Error occurred while fetching weather data');
+        },
+      });
+  }
 }
--- WeatherApp/src/app/app.module.ts
+++ WeatherApp/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
